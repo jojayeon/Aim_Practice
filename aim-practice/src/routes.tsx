f1fab236e4c542922800a5e10148a898800fc648
jojayeon <jojayeon6152@gmail.com>
2025-06-03 23:13:15 +0900

    :package: routes 기본 구조 설정

---
+++ aim-practice/src/routes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import GamePage from './pages/GamePage';
+import ResultPage from './pages/ResultPage';
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+      <Route path="/game" element={<GamePage />} />
+      <Route path="/result" element={<ResultPage />} />
+    </Routes>
+  );
+}
