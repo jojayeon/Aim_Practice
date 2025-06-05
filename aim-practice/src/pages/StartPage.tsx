3c15c4a3bce5a5a4110aa9131c1851175dc54967
jojayeon <jojayeon6152@gmail.com>
2025-06-05 18:07:15 +0900

    :sparkles: start페이지 기초 작성

--- aim-practice/src/pages/StartPage.tsx
+++ aim-practice/src/pages/StartPage.tsx
@@ -1,1 +1,30 @@
 // 시작메인 페이지
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>에임 연습 - 난이도 및 감도 설정</h1>
+      
+      {/* 간단하게 난이도 버튼 3개 */}
+      <div style={{ marginBottom: '20px' }}>
+        <Button style={{ marginRight: '10px' }}>Easy</Button>
+        <Button style={{ marginRight: '10px' }}>Normal</Button>
+        <Button>Hard</Button>
+      </div>
+
+      {/* 감도 슬라이더 자리 (나중에 구현) */}
+      <div style={{ marginBottom: '20px' }}>
+        감도: <input type="range" min="1" max="10" defaultValue="5" />
+      </div>
+
+      <Button onClick={() => navigate('/game')}>시작하기</Button>
+    </div>
+  );
+};
+
+export default HomePage;
