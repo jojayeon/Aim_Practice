41c87ce182e85fb2a92a8a1d80ec904716d41d82
jojayeon <jojayeon6152@gmail.com>
2025-06-05 18:37:44 +0900

    :package: 크로스 헤어 추가

---
+++ aim-practice/src/components/Crosshair.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/Crosshair.module.css';
+
+const Crosshair: React.FC = () => {
+  return <div className={styles.crosshair}></div>;
+};
+
+export default Crosshair;
