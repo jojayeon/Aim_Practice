dcda28504306c5c7213b25d7fe80f5465ada73b7
jojayeon <jojayeon6152@gmail.com>
2025-06-05 18:05:53 +0900

    :sparkles: 기본 페이지 라우팅

--- aim-practice/src/App.tsx
+++ aim-practice/src/App.tsx
@@ -1,12 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import GamePage from './pages/GamePage';
+import ResultPage from './pages/ResultPage';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
