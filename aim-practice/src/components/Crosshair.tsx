5adf00b7f92ef83d5a1f8add7271d3ca81b96002
jojayeon <jojayeon6152@gmail.com>
2025-06-10 17:19:40 +0900

    :fire: 크로스 헤어 삭제 마우스로 대체

--- aim-practice/src/components/Crosshair.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from '../styles/Crosshair.module.css';
-
-const Crosshair: React.FC = () => {
-  return <div className={styles.crosshair}></div>;
-};
-
-export default Crosshair;
